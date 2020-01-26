6c2da4fc4658dbc0d986525f3c9830e6ea9e23c1
talalalamdar <talalalamdar@gmail.com>
2020-01-26 14:02:18 +0100

    fwe

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import io from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io'
 
+const DOMAIN = process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'https://talalalamdar-chatapp.surge.sh';
+
 Vue.use(new VueSocketIO({
-  connection: io('http://localhost:3333')
+  connection: io(DOMAIN)
 }));
 
 Vue.config.productionTip = false;
