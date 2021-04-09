b4033c70a101aaf8ccad6cac89b61936fedf7899
Talal Alamdar <talalalamdar@Talals-MBP.home>
2021-04-09 10:21:45 +0200

    fixing the socket path for production

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import io from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io'
 
-const DOMAIN = process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'https://talalalamdar-chatapp.surge.sh';
+const DOMAIN = process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : '/';
 
 Vue.use(new VueSocketIO({
   connection: io(DOMAIN)
